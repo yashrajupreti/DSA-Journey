9a040d1afe03a4fa58ab97dde244e4bab9758876
yashrajupreti <yashrajupreti9@gmail.com>
2026-09-24 19:07:37 +0530

    Add LeetCode 14 Longest Common Prefix solution
    Implemented a solution for finding the longest common prefix among an array of strings.

---
+++ Arrays/14_Longest_Common_Prefix.cpp
@@ -0,0 +1,21 @@
+// LeetCode 14 - Longest Common Prefix
+// Time Complexity: O(n*m)
+// Space Complexity: O(1)
+//code
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string prefix = strs[0];
+        for(int i = 1 ;i < strs.size(); i++){
+            int j = 0;
+            while( j < prefix.length() && j < strs[i].length() && prefix[j] == strs[i][j])
+            {
+                j++;
+            }
+            prefix = prefix.substr(0 , j);
+            if(prefix.empty())
+            return "";
+        }
+        return prefix;
+    }
+};
